8c7ae710382d1a2a7e810ad366349bca981c6a03
Kieran <v0l@users.noreply.github.com>
2017-06-22 22:02:12 +0800

    update script add rtmp module

--- nginx-latest.sh
+++ nginx-latest.sh
@@ -3,17 +3,20 @@
 apt-get update
 apt-get install build-essential libpcre3-dev libz-dev
 
-OPENSSL_V="openssl-1.0.2h"
-NGINX_V="nginx-1.9.15"
+OPENSSL_V="openssl-1.0.2l"
+NGINX_V="nginx-1.13.1"
 
 wget https://www.openssl.org/source/$OPENSSL_V.tar.gz
 tar xfv $OPENSSL_V.tar.gz
 
 wget http://nginx.org/download/$NGINX_V.tar.gz
 tar xfv $NGINX_V.tar.gz
+
+git clone https://github.com/arut/nginx-rtmp-module.git
+
 cd $NGINX_V
 
-./configure --with-openssl=../$OPENSSL_V --with-ipv6 --with-threads --with-http_ssl_module --with-http_v2_module
+./configure --with-openssl=../$OPENSSL_V --add-module=../nginx-rtmp-module --with-ipv6 --with-threads --with-http_ssl_module --with-http_v2_module
 make -j8
 make install
 
