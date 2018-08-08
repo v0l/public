e2286b9642c2bcaa378d9b1dd98dd42c91e053a2
Kieran <kieran@harkin.me>
2018-08-08 16:18:34 +0800

    keep retrying if nginx stops

--- rrdtool_scripts/collect_nginx_strace.py
+++ rrdtool_scripts/collect_nginx_strace.py
@@ -7,12 +7,14 @@ from subprocess import call, Popen, PIPE
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-ln=0
-proc = Popen(['strace', '-p', sys.argv[1], '-e', 'sendfile'], stdout=PIPE, stderr=PIPE)
-for line in iter(proc.stderr.readline,''):
-	ps = re.search('= ([0-9]{1,20})$', line)
-	if ps != None:
-		print call(['rrdupdate', 'procs/nginx.rrd', '-t', 'out', 'N:' + ps.group(1)])
-	
-	if ++ln % 10 == 0:
-		print call([dir_path + '/nginx_graph_strace.sh'])
+while True:
+	nginx_pid = Popen('pgrep -P $(cat ~/.nginx/pid)', stdout=PIPE, shell=True)
+	ln=0
+	proc = Popen(['strace', '-p', nginx_pid.stdout.read(), '-e', 'sendfile'], stdout=PIPE, stderr=PIPE)
+	for line in iter(proc.stderr.readline,''):
+		ps = re.search('= ([0-9]{1,20})$', line)
+		if ps != None:
+			print call(['rrdupdate', 'procs/nginx.rrd', '-t', 'out', 'N:' + ps.group(1)])
+		
+		if ++ln % 10 == 0:
+			print call([dir_path + '/nginx_graph_strace.sh'])
