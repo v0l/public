f8af42f325a0ea0483b839ae70446375c4a362c6
Kieran <kieran@harkin.me>
2019-10-14 00:50:48 +0800

    Update nginx-latest.sh

--- nginx-latest.sh
+++ nginx-latest.sh
@@ -3,8 +3,8 @@
 apt-get update
 apt-get install build-essential libpcre3-dev libz-dev
 
-OPENSSL_V="openssl-1.1.1a"
-NGINX_V="nginx-1.15.7"
+OPENSSL_V="openssl-1.1.1d"
+NGINX_V="nginx-1.17.4"
 
 wget https://www.openssl.org/source/$OPENSSL_V.tar.gz
 tar xfv $OPENSSL_V.tar.gz
