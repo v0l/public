5f74b277125611baff158af0694dfab440a1ba1a
Kieran <kieran@harkin.me>
2020-05-23 17:05:30 +0800

    Update nginx-rtmp-latest.sh

--- nginx-rtmp-latest.sh
+++ nginx-rtmp-latest.sh
@@ -3,8 +3,8 @@
 apt-get update
 apt-get install build-essential libpcre3-dev libz-dev git
 
-OPENSSL_V="openssl-1.1.0h"
-NGINX_V="nginx-1.15.0"
+OPENSSL_V="openssl-1.1.1g"
+NGINX_V="nginx-1.18.0"
 
 wget https://www.openssl.org/source/$OPENSSL_V.tar.gz
 tar xfv $OPENSSL_V.tar.gz
@@ -21,3 +21,25 @@ make -j8
 make install
 
 ln /usr/local/nginx/sbin/nginx /usr/sbin/nginx
+
+cat >/etc/systemd/system/nginx.service <<EOF
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=syslog.target network-online.target remote-fs.target nss-lookup.target
+Wants=network-online.target
+
+[Service]
+Type=forking
+PIDFile=/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t
+ExecStart=/usr/sbin/nginx
+ExecReload=/usr/sbin/nginx -s reload
+ExecStop=/bin/kill -s QUIT $MAINPID
+PrivateTmp=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable --now nginx
